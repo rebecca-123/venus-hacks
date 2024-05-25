7af9612090c971e666e73be79b717a017a398679
eriyoungg <erikayoung314@gmail.com>
2024-05-25 16:19:38 -0700

    progress on website

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+export { default } from './Navbar';
+
+//ReactDOM.render(
+  //<React.StrictMode>
+  //  <App />
+  //</React.StrictMode>,
+ // document.getElementById('root')
+//);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+   <BrowserRouter>
+   <Routes>
+    <Route path = "/" element={<App/>}/>
+   </Routes>
+   </BrowserRouter>
+ </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
