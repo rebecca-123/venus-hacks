663878d039d27967274bcc7158283f4700458895
Jaylene Nguyen <jaylenen@uci.edu>
2024-05-25 18:50:02 -0700

    Added manual user location input & flask endpoint

--- backend/user_location_auto.py
+++ backend/user_location_auto.py
@@ -3,38 +3,38 @@ import requests
 import json
 
 class UserLocation:
-    """User info to find nearby healthcare providers."""
+    """Automatically get user info to find nearby healthcare providers."""
 
     def __init__(self):
-        self.zip_code, self.state = self._get_postal_and_state()
+        self.state, self.zip_code = self._get_state_zip()
         self.limit = 0
         self.search = {"limit": self.limit, "taxonomy_description" : "Obstetrics & Gynecology", 
-                       "postal_code" : self.zip_code, "state" : self.state}
+                       "state" : self.state, "postal_code" : self.zip_code}
 
     def _get_location(self):
         geo_lookup = GeoLookup("921a4e0e6b2883cdb65b92a7298ecbfe") 
 
         return geo_lookup.get_own_location()
 
-    def _get_postal_and_state(self):
+    def _get_state_zip(self):
         location = self._get_location()
         zip_code = location["zip"]
         state = location["region_code"]
-        return zip_code, state
+        return state, zip_code
 
     def find_providers(self, limit):
         url = "https://npiregistry.cms.hhs.gov/api/?version=2.1"
-        self.search[self.limit] = limit
+        self.search["limit"] = limit
         providers = requests.get(url, params=self.search)
         if providers.status_code == 200:
             providers = json.loads(providers.text)
             results = providers["results"]
-            if len(results) < 1:
-                print("No doctors found nearby.")
+            if not results:
+                yield "No doctors found nearby.\n"
             else:
-                self._parse_locations(results)
+                yield from self._parse_locations(results)
         else:
-            print("Failed to retrieve data: ", providers.status_code)
+            yield "Failed to retrieve data: {providers.status_code}\n"
 
     def _parse_locations(self, providers_dict):
         if providers_dict[0]["addresses"][0]["state"] != self.state:
@@ -50,6 +50,4 @@ class UserLocation:
                         city = addresses[j]["city"]
                         state = addresses[j]["state"]
                         postal = addresses[j]["postal_code"]
-                        return (street, city, state, postal)
-
-
+                        yield f"{street}\n{city}, {state} {postal[0:5]}-{postal[5:]}"
