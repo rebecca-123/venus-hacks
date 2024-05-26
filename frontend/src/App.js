729e4b95e6bef64b5fa06990ebc66fc1a29efccf
Rebecca-123 <rebecca.notw@gmail.com>
2024-05-25 17:16:36 -0700

    frontend form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import Navbar from "./Navbar";
@@ -7,8 +6,7 @@ import "./App.css";
 // import Maternal Health from "./MaternalHealth";
 // import Services from "./Services";
 
-
-function App() {
+export default function App() {
   // Define state for data
   const[name, setName] = useState("");
   const[age, setAge] = useState("");
@@ -39,8 +37,8 @@ function App() {
           age: data.age,
           systolicBP: data.systolicBP,
           diastolicBP: data.diastolicBP,
-		  bloodSugar: data.bloodSugar,
-		  heartRate: data.heartRate,
+		      bloodSugar: data.bloodSugar,
+		      heartRate: data.heartRate,
         });
       })
       .catch(error => {
@@ -62,47 +60,45 @@ function App() {
     //history.push('/results.js');
   }
   return (
-      <div className="App">
-        <header className="App-header">
-          <h1>MaterniCheck</h1>
-          {/* User input form */}
-		<Navbar></Navbar>
-		<br></br>
-		<form onSubmit={handleSubmit}>
-			<div className="body-box">
-		<p>Name: </p>
-		<input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
-		<p>Age: </p>
-		<input type="text" value={age} onChange={(e) => setAge(e.target.value)}/>
-		<p>Upper Value of Blood Pressure (mmHG): </p>
-		<input type="text" value={systolicBP} onChange={(e) => setUpperBP(e.target.value)}/>
-		<p>Lower Value of Blood Pressure (mmHg): </p>
-		<input type="text" value={diastolicBP} onChange={(e) => setLowerBP(e.target.value)}/>
-		<p>Blood glucose levels (mmol/L): </p>
-		<input type="text" value={bloodSugar} onChange={(e) => setBloodSugar(e.target.value)}/>
-		<p>Heart Rate: </p>
-		<input type="text" value={heartRate} onChange={(e) => setHeartRate(e.target.value)}/>
-		</div>
-		<input type="submit"/>
-		</form>
-		{showResults && (
-          <div className="results-box">
-            <h2>Results</h2>
-            <p>Name: {name}</p>
-            <p>Age: {age}</p>
-            <p>Systolic BP: {systolicBP}</p>
-            <p>Diastolic BP: {diastolicBP}</p>
-            <p>Blood Sugar: {bloodSugar}</p>
-            <p>Heart Rate: {heartRate}</p>
-          </div>
-        )}
-        {/* <Route path="/about" element={About/>}/>
-        <Route path="/about" element={Maternal Health/>}/>
-        <Route path="/about" element={Services/>}/> */}
-        </header>
-		</div>
+    <p1>Help</p1>
+    //   <div className="App">
+    //     <header className="App-header">
+    //       <h1>MaterniCheck</h1>
+    //       {/* User input form */}
+		// <Navbar></Navbar>
+		// <br></br>
+		// <form onSubmit={handleSubmit}>
+		// 	<div className="body-box">
+		// <p>Name: </p>
+		// <input type="text" value={name} onChange={(e) => setName(e.target.value)}/>
+		// <p>Age: </p>
+		// <input type="text" value={age} onChange={(e) => setAge(e.target.value)}/>
+		// <p>Upper Value of Blood Pressure (mmHG): </p>
+		// <input type="text" value={systolicBP} onChange={(e) => setUpperBP(e.target.value)}/>
+		// <p>Lower Value of Blood Pressure (mmHg): </p>
+		// <input type="text" value={diastolicBP} onChange={(e) => setLowerBP(e.target.value)}/>
+		// <p>Blood glucose levels (mmol/L): </p>
+		// <input type="text" value={bloodSugar} onChange={(e) => setBloodSugar(e.target.value)}/>
+		// <p>Heart Rate: </p>
+		// <input type="text" value={heartRate} onChange={(e) => setHeartRate(e.target.value)}/>
+		// </div>
+		// <input type="submit"/>
+		// </form>
+		// {showResults && (
+    //       <div className="results-box">
+    //         <h2>Results</h2>
+    //         <p>Name: {name}</p>
+    //         <p>Age: {age}</p>
+    //         <p>Systolic BP: {systolicBP}</p>
+    //         <p>Diastolic BP: {diastolicBP}</p>
+    //         <p>Blood Sugar: {bloodSugar}</p>
+    //         <p>Heart Rate: {heartRate}</p>
+    //       </div>
+    //     )}
+    //     {/* <Route path="/about" element={About/>}/>
+    //     <Route path="/about" element={Maternal Health/>}/>
+    //     <Route path="/about" element={Services/>}/> */}
+    //     </header>
+		// </div>
   );
 }
-
-export default App;
-
